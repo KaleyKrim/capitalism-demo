60bad756311c88aec798c2bce92b8262bd3b0cdb
Kaley Kriminger <kaley.kriminger@gmail.com>
2020-05-10 12:15:52 -0700

    fix decimals

--- client/src/modules/calculation.ts
+++ client/src/modules/calculation.ts
@@ -1,14 +1,31 @@
-import { BusinessStats } from "./localStorage";
+import { BusinessStats } from "../types";
+import { BUSINESS_MASTER_DATA } from "../types/constants";
 
-export function calculateEarnedWhileAway(timePassed: number, businessStats: BusinessStats[]) {
+export function calculateEarnedWhileAway(businessStats: BusinessStats[]) {
 	let earned = 0;
 	for (const business of businessStats) {
 		if (!business.isAutomated) {
 			continue;
 		}
-
-		const harvestsMissed = Math.floor(timePassed / business.msPerHarvest);
+		const timeSinceLastCollected = Math.round((Date.now() / 1000)) - business.collectedAt;
+		const harvestsMissed = Math.floor(timeSinceLastCollected / business.msPerHarvest);
 		earned += (harvestsMissed * business.coinsEarnedPerHarvest);
 	}
 	return earned;
+}
+
+export function getNewStatsForLevelUp(businessId: string, currentStats: BusinessStats): Partial<BusinessStats> {
+	const newLevel = currentStats.level + 1;
+	const speedBonusFactor = BUSINESS_MASTER_DATA[businessId].speedBonusLevels[newLevel];
+	const newStats = {
+		level: newLevel,
+		coinsEarnedPerHarvest: parseFloat((currentStats.coinsEarnedPerHarvest * BUSINESS_MASTER_DATA[businessId].increasedEarningsPerLevelUp).toFixed(2)),
+		coinsToLevelUp: parseFloat((currentStats.coinsToLevelUp * BUSINESS_MASTER_DATA[businessId].levelUpCostIncrease).toFixed(2)),
+	};
+
+	if (speedBonusFactor) {
+		newStats["msPerHarvest"] = parseFloat((currentStats.msPerHarvest * speedBonusFactor).toFixed(2));
+	}
+
+	return newStats;
 }
