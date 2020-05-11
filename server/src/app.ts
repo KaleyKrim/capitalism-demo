94aa568857bd49c389629ee3775a4d40bef44be1
Kaley Kriminger <kaley.kriminger@gmail.com>
2020-05-10 20:42:20 -0700

    add logic for post /scores

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,16 +6,17 @@ import * as path from "path";
 import cors from "cors";
 import bodyParser from "body-parser";
 
+const PORT = process.env.PORT || 3000;
 const app: express.Application = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 app.post("/api/users", async(req, res) => {
-	const userModule = new User();
-	const nickname = req.body.nickname.length ? req.body.nickname : "anon";
+	const nickname = req.body.nickname.length ? req.body.nickname : "no name";
 	
 	let userId: string;
 	try {
+		const userModule = new User();
 		userId = await userModule.create(nickname);
 	} catch (e) {
 		res.send({ error: e })
@@ -28,21 +29,38 @@ app.post("/api/users", async(req, res) => {
 app.get("/api/scores", async(_req, res) => {
 	const scoreModule = new Score();
 	const ranking = await scoreModule.getAll();
-	res.send({ ranking });
+	
+	const userModule = new User();
+	const rankingByNicknames: {name: string; score: number}[] = [];
+	for (const userId in ranking) {
+		const nickname = await userModule.getNickname(userId);
+		if (!nickname) {
+			continue;
+		}
+		rankingByNicknames.push({ name: nickname, score: ranking[userId] });
+	}
+
+	res.send({ scores: rankingByNicknames });
 });
 
 app.post("/api/scores", authenticator, async(req, res) => {
 	const userId = <string>req.body.userId;
 	const score = req.body.score;
 
+	const userModule = new User();
+	const userExists = await userModule.exists(userId);
+	if (!userExists) {
+		res.send({ error: "user with that user id doesn't exist"});
+		return;
+	}
+
 	const scoreModule = new Score();
 	const rank = await scoreModule.set(userId, score);
-
 	res.send({ rank });
 });
 
 app.use("/", express.static(path.join(__dirname, "..", "..", "client", "dist")));
 
-app.listen(3000, () => {
-	console.log("listening on port");
+app.listen(PORT, () => {
+	console.log(`listening on port ${PORT}`);
 });
